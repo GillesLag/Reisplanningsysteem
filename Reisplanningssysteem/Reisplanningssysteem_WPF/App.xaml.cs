1b887812d89721ed178576b8014688bf48778209
Gilles Lagrillière <gilles.lagrilliere@gmail.com>
2023-04-04 20:22:28 +0200

    opzet project

--- Reisplanningssysteem/Reisplanningssysteem_WPF/App.xaml.cs
+++ Reisplanningssysteem/Reisplanningssysteem_WPF/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Reisplanningssysteem_WPF.ViewModels;
 
 namespace Reisplanningssysteem_WPF
 {
@@ -13,5 +14,12 @@ namespace Reisplanningssysteem_WPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainViewModel viewmodel = new MainViewModel();
+            Views.MainView view = new Views.MainView();
+            view.DataContext = viewmodel;
+            view.Show();
+        }
     }
 }
