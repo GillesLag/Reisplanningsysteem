42245121b91ff55556ee8361731aeaccf2a4c71a
r0895226 <46358235+DriesVanHool@users.noreply.github.com>
2023-04-09 19:04:23 +0200

    c

---
+++ Reisplanningssysteem/Reisplanningssysteem_Models/Partials/Gemeente.cs
@@ -0,0 +1,35 @@
+﻿using Reisplanningssysteem_Models.Partials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reisplanningssysteem_Models
+{
+    public partial class Gemeente : BasisKlasse
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(Naam) && string.IsNullOrWhiteSpace(Naam))
+                {
+                    return "Geef een naam in!";
+                }
+
+                if (columnName == nameof(Postcode) && string.IsNullOrWhiteSpace(Postcode))
+                {
+                    return "geef een postcode in ";
+                }
+
+                return "";
+            }
+        }
+
+        public override string? ToString()
+        {
+            return Naam;
+        }
+    }
+}
